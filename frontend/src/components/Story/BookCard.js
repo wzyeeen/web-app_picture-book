69a885b1b0b2fc8832dc391c93e98958e3cb1d2e
James0421 <108306037@nccu.edu.tw>
2023-12-09 17:06:29 +0800

    update bookcard

--- frontend/src/components/Story/BookCard.js
+++ frontend/src/components/Story/BookCard.js
@@ -5,13 +5,13 @@ import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Typography from '@mui/joy/Typography';
 import story from "../../Assets/story.jpg";
+import back from "../../Assets/back.png";
+import next from "../../Assets/next.png";
 import Button from "react-bootstrap/Button";
+import IconButton from '@mui/joy/IconButton';
 import { CgPen } from "react-icons/cg";
 import Textarea from '@mui/joy/Textarea';
 import { AiFillCaretRight } from "react-icons/ai";
-
-import getImage from './api/get-image';
-import getText from './api/get-text';
 
 function Book(props) {
   const [isEditing, setIsEditing] = useState(false);
@@ -20,6 +20,7 @@ function Book(props) {
   const [isLoading, setIsLoading] = useState(false);
   const [prompt, setPrompt] = useState("");
   const [answer, setAnswer] = useState("");
+  const [page, setPage] = useState(1);
 
   const handleEditClick = () => {
     setIsEditing(!isEditing);
@@ -37,20 +38,47 @@ function Book(props) {
     setIsEditing(false);
   };
 
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      let newPage = page - 1;
+      setEditedContent('Your story content ' + newPage);
+      setPage(newPage);
+      setAnswer(answer);
+    }
+  }
+
+  const handleNextPage = () => {
+    if (page < 8) {
+      let newPage = page + 1;
+      setEditedContent('Your story content ' + newPage);
+      setPage(newPage);
+      setAnswer(answer);
+    }
+  }
+
   // New function to interact with chat-gpt API
-  const getChatGPTResponseImage = async (e) => {
+  const getChatGPTResponse = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    
     // Replace with your API endpoint
-    //const apiUrl = "/api/get-image";
+    const apiUrl = "/api/get-image";
 
     try {
-      const data = await getImage(prompt);
-      if (data == "https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_1722.jpg") {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ prompt: prompt })
+      });
+
+      if (!response.ok) {
         throw new Error("Error fetching data from chat-gpt API");
       }
-      setAnswer(data);
+
+      const data = await response.json();
+      setAnswer(data.text);
       setIsLoading(false);
       // Handle the response data as needed
       console.log("ChatGPT Response:", data);
@@ -58,24 +86,6 @@ function Book(props) {
       console.error("Error:", error);
     }
   };
-
-  const getChatGPTResponseText = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    try {
-      const data = await getText(prompt);
-      if (data == "Invalid prompt provided.") {
-        throw new Error("Error fetching data from chat-gpt API");
-      }
-      setAnswer(data);
-      setIsLoading(false);
-      // Handle the response data as needed
-      console.log("ChatGPT Response:", data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
   const handleChange = (e) => {
     setPrompt(e.target.value);
   }
@@ -83,15 +93,18 @@ function Book(props) {
   return (
     <Card className="book-card-view" orientation="horizontal">
       <CardOverflow>
+        <IconButton onClick={handlePreviousPage} style={{backgroundColor: 'transparent'}}>
+          <img src={back} alt={back} width='70px' height='70px' />
+        </IconButton>
         <AspectRatio ratio="1" sx={{ width: 600, margin: '0 16px' }}>
           <img
             src={story}
             alt={story}
           />
         </AspectRatio>
-        <div style={{ position: 'absolute', bottom: '8px', right: '-630px', color: 'white' }}>
+        <div style={{ position: 'absolute', bottom: '8px', right: '-530px', color: 'white' }}>
           <Typography variant="caption" fontWeight="md" textColor="success.plainColor">
-            1 {/* Here you might dynamically calculate or use another way to get the page number */}
+            {page} {/* Here you might dynamically calculate or use another way to get the page number */}
           </Typography>
         </div>
       </CardOverflow>
@@ -118,18 +131,19 @@ function Book(props) {
       >
         <CgPen /> &nbsp; {isEditing ? "Save" : "Edit"}
       </Button>
-      <form className="our-form" onSubmit={getChatGPTResponseImage}>
+      <form className="our-form" onSubmit={getChatGPTResponse}>
         <input className="prompt-field" type="text" onChange={handleChange} />
-        <button className="prompt-button">Go!</button>
+        <Button className="prompt-button">Go!</Button>
       </form>
-      <form className="our-form" onSubmit={getChatGPTResponseText}>
-        <input className="prompt-field" type="text" onChange={handleChange} />
-        <button className="prompt-button">Go!</button>
-      </form>
+
+      <IconButton onClick={handleNextPage} style={{backgroundColor: 'transparent'}}>
+        <img src={next} alt={next} width='70px' height='70px' />
+      </IconButton>
 
       {isLoading && <div className="loading-spinner"></div>}
 
       {isLoading === false && <img src={answer} />}
+
     </Card>
   );
 }
