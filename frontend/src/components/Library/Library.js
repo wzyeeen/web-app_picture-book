611dcd643f1737b6a712ca156151729e24b6082d
nopupu <apple912162@gmail.com>
2023-12-10 19:58:12 +0800

    library get api

--- frontend/src/components/Library/Library.js
+++ frontend/src/components/Library/Library.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import LibraryCard from "./LibraryCards";
@@ -11,9 +11,22 @@ import cartoon from "../../Assets/Styles/cartoon.jpg"
 import watercolor from "../../Assets/Styles/watercolor.png"
 import scifi from "../../Assets/Styles/scifi.jpg"
 import sketch from "../../Assets/Styles/sketch.jpg"
+import axios from "axios";
 
 function Library() {
   const ariaLabel = { 'aria-label': 'description' };
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://web-app-backend-r3ac.onrender.com/book")
+      .then((res) => {
+        setBooks(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -34,72 +47,17 @@ function Library() {
           Here are a few projects I've worked on recently.
         </p> */}
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={sketch}
-              isBlog={false}
-              title="Sketch"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={cartoon}
-              isBlog={false}
-              title="Cartoon"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={anime}
-              isBlog={false}
-              title="Anime"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={oilpainting}
-              isBlog={false}
-              title="Oil Painting"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={watercolor}
-              isBlog={false}
-              title="Water Color"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-            // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <LibraryCard
-              imgPath={scifi}
-              isBlog={false}
-              title="SciFi"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          {books.map(book => (
+            <Col key={book.id} md={4} className="project-card">
+              <LibraryCard
+                imgPath={book.pages[0].image_url} // Assuming the first page image represents the book
+                isBlog={false}
+                title={book.book_name}
+                description={`Thumb Count: ${book.thumb}`}
+              // Include other properties you want to display
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
