66ea9d65192e86d0041d823aeef7eea167e859e7
wzyeeen <71961144+wzyeeen@users.noreply.github.com>
2023-12-15 16:07:13 +0800

    Minor modified of unnecessary scripts

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>Group I</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
